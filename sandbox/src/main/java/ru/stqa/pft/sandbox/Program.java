5ce9788c5b8afa7bb00984e99122bfc6b1341da1
AnnaSkolova <116001368+AnnaSkolova@users.noreply.github.com>
2022-10-20 18:03:05 +0300

    добавлено несколько функций

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Program.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Program.java
@@ -2,6 +2,22 @@ package ru.stqa.pft.sandbox;
 
 public class Program {
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+		hello("user");
+		double len = 5;
+		System.out.println("площадь кв со стороной " + len + " + " + area(len));
+		double a = 2;
+		double b = 3;
+		System.out.println(area(a,b));
+	}
+
+	public static void hello(String somebody){
+		System.out.println("Hello, " + somebody + "!");
+	}
+
+	public static double area (double l){
+		return  l * l;
+	}
+	public static double area (double a, double b){
+		return  a * b;
 	}
 }
